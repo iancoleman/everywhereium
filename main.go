d3f418110dcd28435f40260a970dbab43d3f30a2
Ian Coleman <coleman.ian@gmail.com>
2013-04-18 19:54:16 +1000

    Cache separated from main.go

--- main.go
+++ main.go
@@ -1,62 +1,10 @@
 package main
 
 import (
+    "cache"
     "fmt"
-    "io/ioutil"
     "net/http"
-    "os"
-    "path/filepath"
-    "strings"
-    "strconv"
 )
-
-var cache map[string]string
-
-func getSiteDetail(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "You made it")
-}
-
-func fromCache(w http.ResponseWriter, r *http.Request) {
-    key := strings.Trim(r.URL.Path, "/")
-    fmt.Println(key)
-    if content,ok := cache[key]; ok {
-        fmt.Fprintf(w, content)
-    } else {
-        errorHandler(w, r, http.StatusNotFound)
-    }
-}
-
-func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
-    w.WriteHeader(status)
-    fmt.Fprintf(w, cache[strconv.Itoa(status)])
-}
-
-func loadCache() {
-    cache = make(map[string]string)
-
-    err := filepath.Walk("static", cacheStaticFile)
-    if err != nil {
-        panic(err)
-    }
-}
-
-func cacheStaticFile(filename string, f os.FileInfo, err error) error {
-    if f.IsDir() {
-        return nil
-    }
-    key := strings.Replace(filename, "static/", "", 1)
-    key = key[:strings.LastIndex(key, ".html")]
-    fmt.Println("Caching " + filename + " as " + key)
-    content, err := ioutil.ReadFile(filename)
-    if key == "index" {
-        cache[""] = string(content)
-        http.HandleFunc("/", fromCache)
-        return err
-    }
-    cache[key] = string(content)
-    http.HandleFunc(key, fromCache)
-    return err
-}
 
 func getAddress(w http.ResponseWriter, r *http.Request) {
     // Do some jsonrpc to armoryd
@@ -77,12 +25,8 @@ func search(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    env := os.Getenv("ENV")
-    if env == "development" {
-        http.Handle("/", http.FileServer(http.Dir("static")))
-    } else {
-        loadCache()
-    }
+
+    cache.Start()
 
     http.HandleFunc("/getAddress", getAddress)
     http.HandlerFunc("/rescan", rescan)
---
+++ src/cache/cache.go
@@ -0,0 +1,65 @@
+package cache
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "path/filepath"
+    "strconv"
+    "strings"
+)
+
+var Map map[string]string
+
+func Start() {
+    env := os.Getenv("ENV")
+    if env == "development" {
+        http.Handle("/", http.FileServer(http.Dir("static")))
+    } else {
+        loadCache()
+    }
+}
+
+func loadCache() {
+    Map = make(map[string]string)
+
+    err := filepath.Walk("static", cacheStaticFile)
+    if err != nil {
+        panic(err)
+    }
+}
+
+func cacheStaticFile(filename string, f os.FileInfo, err error) error {
+    if f.IsDir() {
+        return nil
+    }
+    key := strings.Replace(filename, "static/", "", 1)
+    key = key[:strings.LastIndex(key, ".html")]
+    fmt.Println("Caching " + filename + " as " + key)
+    content, err := ioutil.ReadFile(filename)
+    if key == "index" {
+        Map[""] = string(content)
+        http.HandleFunc("/", fromCache)
+        return err
+    }
+    Map[key] = string(content)
+    http.HandleFunc(key, fromCache)
+    return err
+}
+
+func fromCache(w http.ResponseWriter, r *http.Request) {
+    key := strings.Trim(r.URL.Path, "/")
+    fmt.Println(key)
+    if content,ok := Map[key]; ok {
+        fmt.Fprintf(w, content)
+    } else {
+        errorHandler(w, r, http.StatusNotFound)
+    }
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+    w.WriteHeader(status)
+    fmt.Fprintf(w, Map[strconv.Itoa(status)])
+}
+

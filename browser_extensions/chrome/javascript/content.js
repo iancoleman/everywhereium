1549fda94a7c6648a99d2b3150ace5c1428b4929
Ian Coleman <coleman.ian@gmail.com>
2013-04-20 13:26:21 +1000

    Remote address retrieval has placeholder function

--- browser_extensions/chrome/javascript/content.js
+++ browser_extensions/chrome/javascript/content.js
@@ -3,28 +3,50 @@ var $els = [];
 var recipientEls = [];
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.show) {
-        tips = [];
-        $("[tip]").each(function(i) {
-            try {
-                var $el = $(this);
-                var tipStr = $el.attr("tip");
-                var tip = JSON.parse(tipStr);
-                tips.push(tip);
-                $els.push($el);
-            }
-            catch (e) {}
-        });
+        getTips();
         sendResponse({data: tips});
-        highlightRecipients();
     }
     else if (request.hide) {
         unhighlightRecipients();
     }
 });
 
+function getTips() {
+    tips = [];
+    $("[tip]").each(function(i) {
+        try {
+            var $el = $(this);
+            var tipStr = $el.attr("tip");
+            var tip = JSON.parse(tipStr);
+            tips.push(tip);
+            $els.push($el);
+        }
+        catch (e) {}
+    });
+    if (tips.length == 0) {
+        getRemoteTip();
+    }
+    highlightRecipients();
+}
+
+function getRemoteTip() {
+    tips.push({
+        for: "This website",
+        destinations: [
+            {"bitcoin": "1address"}
+        ]
+    });
+}
+
 function highlightRecipients() {
     for (var i=0; i<$els.length; i++) {
         createRecipientEl($els[i], tips[i].for);
+    }
+}
+
+function unhighlightRecipients() {
+    for (var i=0; i<recipientEls.length; i++) {
+        recipientEls[i].remove();
     }
 }
 
@@ -49,10 +71,4 @@ function createRecipientEl(el, recipient) {
     });
     recipientEls.push(div);
     $("body").append(div);
-}
-
-function unhighlightRecipients() {
-    for (var i=0; i<recipientEls.length; i++) {
-        recipientEls[i].remove();
-    }
 }

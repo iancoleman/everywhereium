b5e2cc6b69a28ff47d84cc616e21784c25949afd
Ian Coleman <coleman.ian@gmail.com>
2013-04-20 13:59:02 +1000

    Popup table button renderers
    Tip property changed to
    {
        for: "me",
        destinations: [
            {
                "type": "bitcoin",
                "address": "1address"
            },
            {
                "type": "paypal",
                "address": "me@example.com"
            }
        ]
    }

--- browser_extensions/chrome/javascript/popup.js
+++ browser_extensions/chrome/javascript/popup.js
@@ -1,8 +1,8 @@
-
  function init(tabId) {
     listenForPopupClosed(tabId);
     chrome.tabs.sendMessage(tabId, {show: true}, function(response) {
         var tips = response.data;
+
         var tipEl = createPopupContent(tips);
         $("body").append(tipEl);
     });
@@ -35,17 +35,44 @@ function renderTable(tips) {
     // requires. boourns.
     var table = "" +
     "<table class='table-striped'>" +
-    "<tr><th>Recipient</th><th>Tip using</th></tr>";
+    "<tr><th>Recipient</th><th colspan='99'>Tip using</th></tr>";
     for (var i=0; i<tips.length; i++) {
+        var tip = tips[i];
         table += "<tr>" +
             "<td>" +
-                tips[i].for.replace(/\s/g,"&nbsp;") +
+                tip.for.replace(/\s/g,"&nbsp;") +
             "</td>" +
-            "<td>" +
-                "Something" +
-            "</td>" +
-        "</tr>"
+            renderDestinationButtons(tip) +
+        "</tr>";
     }
     table += "</table>";
     return table
+}
+
+function renderBitcoinButton(destination) {
+    return "BITCOIN";
+}
+
+function renderPaypalButton(destination) {
+    return "PAYPAL";
+}
+
+var buttonRenderers = {
+    "bitcoin": renderBitcoinButton,
+    "paypal": renderPaypalButton
+}
+
+function renderDestinationButtons(tip) {
+    var buttonCells = "";
+    var destinations = tip.destinations;
+    for (var j=0; j<destinations.length; j++) {
+        var destination = destinations[j];
+        if (destination.type in buttonRenderers) {
+            buttonCells += "" +
+            "<td>" +
+                buttonRenderers[destination.type](destination) +
+            "</td>";
+        }
+    }
+    return buttonCells;
 }

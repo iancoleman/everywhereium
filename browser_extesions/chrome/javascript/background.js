99cc87743dd03152a1133263376e2f11ed702537
Ian Coleman <coleman.ian@gmail.com>
2013-04-20 11:13:52 +1000

    Popup close event detected without polling

--- browser_extesions/chrome/javascript/background.js
+++ browser_extesions/chrome/javascript/background.js
@@ -1,16 +1,3 @@
-function popupOpened() {
-    var popupWatcher = setInterval(function() {
-        var popup = chrome.extension.getViews({type: "popup"});
-        if (popup.length == 0) {
-            clearInterval(popupWatcher);
-            popupClosed();
-        }
-    }, 100);
-}
-
-function popupClosed() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
-        var tabId = tab[0].id;
-        chrome.tabs.sendMessage(tabId, {hide: true}, null);
-    });
+function popupClosed(tabId) {
+    chrome.tabs.sendMessage(tabId, {hide: true}, null);
 }
--- browser_extesions/chrome/javascript/popup.js
+++ browser_extesions/chrome/javascript/popup.js
@@ -1,5 +1,6 @@
 
  function init(tabId) {
+    listenForPopupClosed(tabId);
     chrome.tabs.sendMessage(tabId, {show: true}, function(response) {
         var tips = response.data;
         for (var i=0; i<tips.length; i++) {
@@ -13,12 +14,18 @@
     });
 }
 
+function listenForPopupClosed(tabId) {
+    var background = chrome.extension.getBackgroundPage();
+    addEventListener("unload", function (event) {
+        background.popupClosed(tabId);
+    }, true);
+}
+
 function createPopupContent(tip) {
     return $("<div>" + tip.id.replace(/\s/g,"&nbsp;") + "</div>");
 }
 
 $(function() {
-    chrome.extension.getBackgroundPage().popupOpened();
     chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
         var tabId = tab[0].id;
         init(tabId);

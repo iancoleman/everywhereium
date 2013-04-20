c203266913f009914976604638f07d056eb1708a
Ian Coleman <coleman.ian@gmail.com>
2013-04-20 11:43:14 +1000

    Tip properties can be on any element.

--- browser_extesions/chrome/javascript/content.js
+++ browser_extesions/chrome/javascript/content.js
@@ -4,7 +4,7 @@ var recipientEls = [];
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.show) {
         tips = [];
-        $("div[tip]").each(function(i) {
+        $("[tip]").each(function(i) {
             try {
                 var $el = $(this);
                 var tipStr = $el.attr("tip");
@@ -31,7 +31,7 @@ function highlightRecipients() {
 function createRecipientEl(el, recipient) {
     var div = $(document.createElement("div"));
     div.text("Like this? Tip " + recipient);
-    var borderWidth = 1;
+    var borderWidth = 4;
     var borderThickness = borderWidth * 2;
     var position = el.position();
     div.css({
@@ -41,7 +41,11 @@ function createRecipientEl(el, recipient) {
         top: position.top,
         left: position.left,
         border: borderWidth + "px solid red",
-        textAlign: "right"
+        textAlign: "center",
+        fontSize: "1.6em",
+        fontWeight: "bold",
+        backgroundColor: "rgba(255,255,255,0.3)",
+        padding: "1em 0"
     });
     recipientEls.push(div);
     $("body").append(div);

ab8eec1cfdda90f6355760f1acb314f811dd08c5
Ian Coleman <coleman.ian@gmail.com>
2013-04-20 22:19:22 +1000

    Paypal client saved / loaded on options page

--- browser_extensions/chrome/javascript/options.js
+++ browser_extensions/chrome/javascript/options.js
@@ -5,11 +5,18 @@ function loadOptions() {
     }
     var paypalemail = localStorage["paypalemail"];
     $("#paypalemail").val(paypalemail);
+    var paypalclient = localStorage["paypalclient"];
+    if (paypalclient) {
+        $("#" + paypalclient).prop("checked", true);
+    }
 }
 
 function saveOptions() {
     saveOption("bitcoinclient", $("input[name=bitcoinclient]:checked").val());
     saveOption("paypalemail", $("#paypalemail").val());
+    saveOption("paypalclient", $("input[name=paypalclient]:checked").val());
+    $("#save").text("Saved!")
+    setTimeout(function() { $("#save").text("Save") }, 1000);
 }
 
 function saveOption(key, val) {

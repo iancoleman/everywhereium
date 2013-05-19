0c03ed5bdc8004ef5fba40ab95d6c408dff4c98b
Ian Coleman <coleman.ian@gmail.com>
2013-05-19 18:19:56 +1000

    Simpler caching, was not returning mimetype.

--- server/src/cache/cache.go
+++ server/src/cache/cache.go
@@ -1,13 +1,7 @@
 package cache
 
 import (
-    "fmt"
-    "io/ioutil"
     "net/http"
-    "os"
-    "path/filepath"
-    "strconv"
-    "strings"
 )
 
 var Map map[string]string
@@ -15,56 +9,5 @@ var mux *http.ServeMux
 
 func Start(serveMux *http.ServeMux) {
     mux = serveMux
-    env := os.Getenv("ENV")
-    if env == "development" {
-        mux.Handle("/", http.FileServer(http.Dir("static")))
-    } else {
-        loadCache()
-    }
+    mux.Handle("/", http.FileServer(http.Dir("static")))
 }
-
-func loadCache() {
-    Map = make(map[string]string)
-
-    err := filepath.Walk("static", cacheStaticFile)
-    if err != nil {
-        panic(err)
-    }
-}
-
-func cacheStaticFile(filename string, f os.FileInfo, err error) error {
-    if f.IsDir() {
-        return nil
-    }
-    key := strings.Replace(filename, "static/", "", 1)
-    extension := key[strings.LastIndex(key, "."):]
-    if extension == ".html" {
-        key = key[:strings.LastIndex(key, extension)]
-    }
-    fmt.Println("Caching " + filename + " as " + key)
-    content, err := ioutil.ReadFile(filename)
-    if key == "index" {
-        Map[""] = string(content)
-        mux.HandleFunc("/", fromCache)
-        return err
-    }
-    Map[key] = string(content)
-    mux.HandleFunc(key, fromCache)
-    return err
-}
-
-func fromCache(w http.ResponseWriter, r *http.Request) {
-    key := strings.Trim(r.URL.Path, "/")
-    fmt.Println(key)
-    if content,ok := Map[key]; ok {
-        fmt.Fprintf(w, content)
-    } else {
-        errorHandler(w, r, http.StatusNotFound)
-    }
-}
-
-func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
-    w.WriteHeader(status)
-    fmt.Fprintf(w, Map[strconv.Itoa(status)])
-}
-
